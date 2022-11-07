f6270286df045a1292ba9ed30a6ebbed81e106b3
huynhquoctrung136 <101690599+huynhquoctrung136@users.noreply.github.com>
2022-11-07 09:52:40 +0700

    feat update-library-app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+//Font-Awesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
+//React-Toastify
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
